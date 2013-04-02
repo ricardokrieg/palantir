1db0d5b1abdbc5129339d5f4c87b4bb668057558
Ricardo Franco <ricardo.krieg@gmail.com>
2013-04-02 05:53:16 -0300

    Some user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
     devise :omniauthable, :rememberable, :trackable, :lockable, :timeoutable, omniauth_providers: [:github]
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :uid, presence: true, uniqueness: true
+    validates :provider, presence: true, inclusion: {in: ['github']}
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,9 @@ require 'faker'
 
 FactoryGirl.define do
     factory :user do
-        email {Faker::Internet.email}
-        username {Faker::Internet.user_name}
+        email       {Faker::Internet.email}
+        username    {Faker::Internet.user_name}
+        provider    'github'
+        uid         '123'
     end
 end
