3839a0e03f9d01036a696281b265092d4f72ca54
Ricardo Franco <ricardo.krieg@gmail.com>
2013-04-02 02:05:48 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Palantir::Application.config.secret_key_base = '6061c15c03626724bce9820945e154ddebe5cd4f210497e8295198339afc3ab14d64b7d711ccdd06201da7ae08ecbfcdd922a78d12dedd0c782109522683aef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Palantir::Application.config.session_store :encrypted_cookie_store, key: '_palantir_session'
