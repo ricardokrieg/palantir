6867b5eec8e296c53e52930b4dbe76e26123995a
Ricardo Franco <ricardo.krieg@gmail.com>
2013-04-02 06:14:32 -0300

    Tests for controller pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,11 +4,14 @@ describe PagesController do
 
     describe "GET 'home'" do
         it "returns http success" do
-            get 'home'
+            get :home
             response.should be_success
         end
 
-        it "renders the :home view"
+        it "renders the :home view" do
+            get :home
+            response.should render_template :home
+        end
     end
 
 end
