57fcfa0af2c6e2bc3aa7534abd9ead17d39782d2
Ricardo Franco <ricardo.krieg@gmail.com>
2013-04-02 22:02:34 -0300

    tests for model category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -19,7 +19,7 @@ describe Category do
         FactoryGirl.build(:category, name: nil).should_not be_valid
     end
 
-    it "is invalid with duplicate name" do
+    it "disallows duplicates for name" do
         category = FactoryGirl.create(:category)
 
         FactoryGirl.build(:category, name: category.name).should_not be_valid
